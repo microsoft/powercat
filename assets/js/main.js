1c6e4e51a5272d16d2312e9b3066f916819e4a3a
Robert Standefer <robstand@hey.com>
2025-03-17 16:58:51 -0700

    Added resources page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Prototype by Pixelarity
+	Archetype by Pixelarity
 	pixelarity.com | hello@pixelarity.com
 	License: pixelarity.com/license
 */
@@ -8,8 +8,8 @@
 
 	var	$window = $(window),
 		$body = $('body'),
-		$header = $('#header'),
-		$banner = $('#banner');
+		$banner = $('#banner'),
+		$header = $('#header');
 
 	// Breakpoints.
 		breakpoints({
@@ -27,6 +27,52 @@
 			}, 100);
 		});
 
+	// Mobile?
+		if (browser.mobile)
+			$body.addClass('is-mobile');
+		else {
+
+			breakpoints.on('<=medium', function() {
+				$body.addClass('is-mobile');
+			});
+
+			breakpoints.on('>medium', function() {
+				$body.removeClass('is-mobile');
+			});
+
+		}
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			alignment: 'right',
+			hideDelay: 400
+		});
+
+	// Nav Panel.
+
+		// Toggle.
+			$('<a href="#navPanel" class="navPanelToggle"></a>')
+				.appendTo($header);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+					'<a href="#navPanel" class="close"></a>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right'
+				});
+
 	// Header.
 		if ($banner.length > 0
 		&&	$header.hasClass('alt')) {
@@ -34,25 +80,12 @@
 			$window.on('resize', function() { $window.trigger('scroll'); });
 
 			$banner.scrollex({
-				bottom:		$header.outerHeight(),
+				bottom:		$header.outerHeight() + 5,
 				terminate:	function() { $header.removeClass('alt'); },
 				enter:		function() { $header.addClass('alt'); },
 				leave:		function() { $header.removeClass('alt'); $header.addClass('reveal'); }
 			});
 
 		}
-
-	// Menu.
-		$('#menu')
-			.append('<a href="#menu" class="close"></a>')
-			.appendTo($body)
-			.panel({
-				delay: 500,
-				hideOnClick: true,
-				hideOnSwipe: true,
-				resetScroll: true,
-				resetForms: true,
-				side: 'right'
-			});
 
 })(jQuery);
